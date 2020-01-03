37a2fa437da466563d747c1f1607db5175964eae
s1dav1s <s1dav1s@stanford.edu>
2020-01-03 11:12:43 -0800

    Commented runVapScan.py

--- VapScanFunc.py
+++ VapScanFunc.py
@@ -1,3 +1,11 @@
+"""
+These are the functions you need to load in order to remotely control Bob's power supply.
+Served as starter code for AliceScanFunc.py.
+
+Requirements: Python3, packages listed below
+OS: CentOS7
+"""
+
 import requests
 import ast
 from datetime import datetime, timedelta
@@ -17,21 +25,19 @@ import argparse
 import pyvisa
 import matplotlib.pyplot as plt
 
+
+#Initiates remote control of power supply using VISA
 def InitiateResource():
     VISAInstance=pyvisa.ResourceManager('@py')
-    #ResourceList=VISAInstance.list_resources()
-    #print(ResourceList)
-    #for index in range(len(ResourceList)):
-    #    print("Device number " + str(index) + " - " + ResourceList[index])
-    #DeviceNumber = input("Which device is the power supply? ")
-    #resourceName=ResourceList[int(DeviceNumber)]
-    resourceName="USB0::65535::37168::602361010736720001::0::INSTR"
-    Resource = VISAInstance.open_resource(resourceName)#,write_termination='\n',read_termination='\r')
+    resourceName="USB0::65535::37168::602361010736720001::0::INSTR" #VISA name of power supply
+    Resource = VISAInstance.open_resource(resourceName)
     print(Resource.query("*IDN?"))
     print("Set remote access")
     Resource.write("SYSTEM:REMOTE") #Set device to remote control
     return Resource
 
+
+#Sets the channel number of the power supply
 def SetChannel(Resource, ChannelNumber):
     if ChannelNumber == 1:
         cmd1 = "inst CH1"
@@ -40,14 +46,19 @@ def SetChannel(Resource, ChannelNumber):
     elif ChannelNumber == 3:
         cmd1 = "inst CH3"
     Resource.write(cmd1)
-    #print("Set channel to "+str(ChannelNumber)+"\n")
-#Safety check -- if true, then check that set voltage between 0 and 2.
+
+#Sets voltage of power supply for a given instrument, channel number, and channel voltage
 def SetVoltage(Resource, ChannelNumber, ChVoltage):
     SetChannel(Resource,ChannelNumber)
     cmd2 = "volt " + str(ChVoltage) + "V"
     Resource.write(cmd2)
     return float(Resource.query("MEAS:VOLT?").rstrip())
 
+
+#Creates array of voltages corresponding stairs of voltage: numSteps is the
+#number of steps, where the step is a constant voltage for "sizeStep" iterations.
+#If you plot out the array vs the index number (corresponding to time), the trace
+#looks like stairs
 def VoltageStairs(V1,V2,numSteps,sizeStep):
     vArray = np.linspace(V1,V2,numSteps)
     vStairs=[]
@@ -56,13 +67,25 @@ def VoltageStairs(V1,V2,numSteps,sizeStep):
             vStairs.append(Vap)
     return np.array(vStairs)
 
+
+#Creates array of voltages that corresponds to a voltage ramp. V1 is the starting
+#voltage, V2 is the end voltage, and numV is the number of voltages to get from
+#V1 to V2 in the ramp.
+#If you plot out the array vs the index number (corresponding to time), the trace
+#looks like a line from V1 to V2
 def VoltageRamp(V1,V2,numV):
     return np.linspace(V1,V2,numV)
 
+
+#Creates array of voltages where each element is V. "size" is the number of
+#V's in the array.
+#If you plot out the array vs the index number (corresponding to time), the trace
+#looks like a horizontal line.
 def VoltageConst(V,size):
     return V*np.ones(size)
 
+
+#Turns off remote control of power supply
 def DisableLVOutput(Resource):
     Resource.write("outp off")
-    #Resource.read()
     print("Disabled LV Output")
--- runVapScan.py
+++ runVapScan.py
@@ -1,3 +1,19 @@
+"""
+This script maximizes Bob's IM extinction ratio by minimizing the IM's output
+optical power and prevents drifting.
+It scans over the DC Bias voltage for Bob's intensity modulator (IM) and
+finds the tuning voltage corresponding to the minimum output power, then
+periodically does fine range scans to find and keep the IM at min power.
+
+Bob has a ~50dB IM with two voltage pins (DC Bias and ER Tuning)
+
+
+Requirements: Python3, VapScanFunc.py in same directory, packages listed below
+and in VapScanFunc
+OS: CentOS7
+
+"""
+
 from VapScanFunc import *
 import pymysql
 import pyvisa as visa
@@ -9,51 +25,52 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 #Need to allow permission: sudo chown inqnet4:inqnet4 /dev/usbtmc0
 
+#Sets directory for saving figures produced by this script
 mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname("/home/inqnet4/Desktop/CQNET/IntensityModulator"))
 
 
-
-
-
-db = pymysql.connect(host = "192.168.0.125", #Wired IPv4 Address
-					user ="INQNET4", # this user only has access to CP
-					password="Teleport1536!", # your password
-					database="teleportcommission",
-					charset='utf8mb4',
-					#port = 5025,
-					cursorclass=pymysql.cursors.DictCursor) #name of the data
+#Connect to mysql database
+db = pymysql.connect(host="<IP ADDRESS>",  #Replace <IP ADDRESS> with the IP of computer with database. Local host if is same computer.
+					 user="<USERNAME>", #Replace <USERNAME> with your username
+					 passwd="<PASSWORD>",  #Replace <PASSWORD> with your password
+					 database="teleportcommission",
+					 charset='utf8mb4',
+ 					 cursorclass=pymysql.cursors.DictCursor)
 
 
 
 
-#inst = USBTMC(device="/dev/usbtmc1")
-#powermeter = ThorlabsPM100(inst=inst)
+#Connect to powermeter
 VISAInstance=pyvisa.ResourceManager('@py')
 resourceName='USB0::4883::32888::P0023460::0::INSTR'
 inst=VISAInstance.open_resource(resourceName)
 print(inst.ask("*IDN?"))
 powermeter = ThorlabsPM100(inst=inst)
-#ChannelNumber=2
-numV=400
+
+Vscan = 0.05 #Range of fine scan (V)
+numV=400 #Number of points to divide initial scan range for IM
 Vmin=0 #in Volts
 Vmax=22#in Volts
-Vscan = 0.05
+
+#Connect to powersupply
 VISAInstance=pyvisa.ResourceManager('@py')
 Resource=InitiateResource()
 
-
+#Channel number corresponding to Bob's DC Bias pin
 channelNum = 2
 ChannelNumber=int(channelNum)
 
 
-
+#Create cursor to select data from mysql.
 cur = db.cursor()
+#Option to back up data to textfile
 backup = False
 print("Back up to text file: " + str(backup))
 if backup:
 	txtFile = open("VapVinPIM.txt","w")
 
 
+#Mysql commands to get the index of the last entry of IM table
 query = "SELECT max(id) from IM"
 cur.execute(query)
 result = cur.fetchall()
@@ -66,39 +83,40 @@ i = maxid +1
 
 try:
 	values = [0]*5
-	Vapplied=VoltageRamp(Vmin,Vmax,numV)
+	Vapplied=VoltageRamp(Vmin,Vmax,numV) #Returns array of Vap elements for initial scan of IM algorithm
 	t=np.arange(1,1+len(Vapplied))
 	Vin=[]
 	P=[]
 	Va=[]
 	print('Writing and reading applied/input voltage values, press Ctrl-C to quit...')
+	# Print nice channel column headers.
 	line='  ID  |   Date/Time   |    Bias Voltage Applied (V)    |    Voltage Measured (V)    |    Power (nW)    '.format(*range(5))
 	print(line)
 	if backup:
 		txtFile.write(line+"\n")
 	line='-' * 100
 	print(line)
-	#print(Vapplied[0])
 	SetVoltage(Resource,ChannelNumber,Vapplied[0])
-	time.sleep(5)
+	time.sleep(5) #Give time to settle to first voltage of Vapplied array
 	if backup:
 		txtFile.write(line+"\n")
-	for Vap in Vapplied:
+	for Vap in Vapplied: #Loops through elements in Vapplied array for initial scan, sets power supply to each element.
 		values[0]=str(i)
 		values[1]= str(datetime.now())
 		values[2]="{0:.3f}".format(Vap)
-		SetVoltage(Resource,ChannelNumber,Vap)
+		SetVoltage(Resource,ChannelNumber,Vap) #Set channel voltage of power supply
 		time.sleep(0.05) #Wait
-		vMeas = float(Resource.query("MEAS:VOLT?").rstrip())
+		vMeas = float(Resource.query("MEAS:VOLT?").rstrip())  #Channel voltage reported by power supply
 		Vin.append(vMeas)
 		values[3]=str(vMeas)
-		p=10**9*powermeter.read
+		p=10**9*powermeter.read #power reported by power meter
 		P.append(p)
 		values[4]="{0:.3f}".format(p)
 		line=' {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>6} '.format(*values)
 		print(line)
 		if backup:
 		     txtFile.write(line+"\n")
+		#SQL command to insert data into database
 		query="INSERT INTO IM(datetime, DCVap, DCVin, P) values(NOW(), +"+values[2]+","+values[3]+","+values[4]+");"
 		cur.execute(query)
 		db.commit()
@@ -108,9 +126,9 @@ try:
 	Pmin = np.amin(P)
 	Pmax = np.amax(P)
 	eRatio=-10*np.log10(Pmin/Pmax)
-	print("Exinction Ratio Lower Bound: ", eRatio)
+	print("Exinction Ratio Lower Bound: ", eRatio) #Max extinction ratio (ER) from initial scan
 
-	#Initial scan
+	#Plots Power vs Applied Voltage from initial scan
 	fig, axs = plt.subplots(1,1,num="1")
 	PmW=[]
 	for pnW in P:
@@ -120,69 +138,70 @@ try:
 	axs.grid()
 	axs.set_xlabel("Applied Voltage (V)")
 	axs.set_ylabel(r"Power ($n W$)")
-	figname="InitScan.png"
-	#plt.savefig(figname)
 
 
+
+	#Get index corresponding to min of power from init scan
 	PminIndex = np.where(P==Pmin)
 	PminIndex=PminIndex[0]
-	Va_minP=Vapplied[PminIndex[0]]
+	Va_minP=Vapplied[PminIndex[0]] #Find voltage value corresponding to min power
+
+	#Get index corresponding to max of power from init scan
 	PmaxIndex = np.where(P==Pmax)
 	PmaxIndex=PmaxIndex[0]
-	Va_maxP=Vapplied[PmaxIndex[0]]
+	Va_maxP=Vapplied[PmaxIndex[0]] #Find voltage value corresponding to max power
+
 	print("Va for min P: ",Va_minP)
 	print("Pmin: ",Pmin)
 	print("Va for max P: ",Va_maxP)
 	print("Pmax: ",Pmax)
+
+	#Set powersupply voltage to max power
 	SetVoltage(Resource,ChannelNumber,Va_minP)
-	time.sleep(10)
+	time.sleep(10) #Wait to settle at the max voltage
 	print("Vin after setting Va for min P: ",float(Resource.query("MEAS:VOLT?").rstrip()))
 	print("P (nW): ",10**9*powermeter.read)
 	starttime=datetime.now()
 	curtime=starttime
-	#Ptot=[]
-	#Vatot=[]
-	#Vintot=[]
 	line='  ID  |   Date/Time   |    Voltage Applied (V)    |    Voltage Measured (V)    |    Power (nW)    '.format(*range(5))
 	print(line)
 	line='-' * 100
 	print(line)
 	n=0
-	while True:#for n in range(3):#while True:
+	while True: #Fine scan loop
 		curtime = datetime.now()
 		vMeas = float(Resource.query("MEAS:VOLT?").rstrip())
+		#if at least one second has passed, record data:
 		if (curtime-starttime) > timedelta(seconds=1):
 			starttime=curtime
 			values[0]=str(i)
 			values[1]=str(datetime.now())
-			values[2]="{0:.3f}".format(Va_minP)
-			values[3] = str(vMeas)
-			p=10**9 * powermeter.read
+			values[2]="{0:.3f}".format(Va_minP) #Channel set voltage
+			values[3] = str(vMeas) #Channel voltage as reported by power supply
+			p=10**9 * powermeter.read #Measure power from powermeter
 			values[4]="{0:.3f}".format(p)
+			# Print nice channel column headers.
 			line=' {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>6} '.format(*values)
 			print(line)
 			if backup:
 				txtFile.write(line+"\n")
+			#SQL command to insert data into database
 			query="INSERT INTO IM(datetime, DCVap, DCVin, P) values(NOW(), +"+values[2]+","+values[3]+","+values[4]+");"
 			cur.execute(query)
 			db.commit()
 			i+=1
-		P=[]
-		Vin=[]
-		Vapplied = VoltageRamp(vMeas-Vscan/2, vMeas+Vscan/2,40)
+		P=[] #Array for power measurements from fine scan
+		Vin=[] #Array for channel voltage measurements from fine scan
+		Vapplied = VoltageRamp(vMeas-Vscan/2, vMeas+Vscan/2,40) #See VapScanFunc.py
 		SetVoltage(Resource,ChannelNumber,Vapplied[0])
 		time.sleep(0.1)
+		#Loops through elements in fine scan Vapplied array, sets power supply to each element.
 		for Vap in Vapplied:
-			#if Vap > 20 or Vap < 0:
-			#	time.sleep()
-			#	if Vap < 0:
-			#		Vap = 20 + Vap
-			#Vap = np.mod(Vap,20)
 			SetVoltage(Resource,ChannelNumber,Vap)
-			#time.sleep(0.05)
 			vMeas = float(Resource.query("MEAS:VOLT?").rstrip())
 			Vin.append(vMeas)
 			p=0
+			#Gets average of 10 powermeter measurements in rapid succession
 			for s in range(10):
 				p=10**9*powermeter.read
 				p=p+p
@@ -190,18 +209,13 @@ try:
 			P.append(p)
 		P=np.array(P)
 		Vin=np.array(Vin)
-		Pmin = np.amin(P)
-		#print(Pmin)
-		PminIndex = np.where(P==Pmin)
+		Pmin = np.amin(P) #Get min power from fine scan
+		PminIndex = np.where(P==Pmin) #Get index of min power from fine scan
 		PminIndex=PminIndex[0]
-		#print(PminIndex[0])
-		Va_minP=Vapplied[PminIndex[0]]
-		#Ptot.append(P)
-		#Vintot.append(Vin)
-		#Vatot.append(Vapplied)
-		#print(P)
-		#print(Pmin)
-		SetVoltage(Resource,ChannelNumber,Va_minP)
+		Va_minP=Vapplied[PminIndex[0]] #Get voltage corresponding to min power from fine scan
+		SetVoltage(Resource,ChannelNumber,Va_minP) #Set to voltage of min power from previous scan
+
+		#Plots of extinction ratio over time after various iterations
 		if n == 1:
 			fig, axs = plt.subplots(1,1,num="10")
 			axs.plot(Vapplied,P)
@@ -210,7 +224,6 @@ try:
 			axs.set_ylabel(r"Power ($n W$)")
 			plt.subplots_adjust(hspace=0.4)
 			figname="FineScan0.png"
-			#plt.savefig(figname)
 		if n == 5:
 			fig, axs = plt.subplots(1,1,num="11")
 			axs.plot(Vapplied,P)
@@ -219,7 +232,6 @@ try:
 			axs.set_ylabel(r"Power ($n W$)")
 			plt.subplots_adjust(hspace=0.4)
 			figname="FineScan1.png"
-			#plt.savefig(figname)
 		if n == 10:
 			fig, axs = plt.subplots(1,1,num="12")
 			axs.plot(Vapplied,P)
@@ -228,7 +240,6 @@ try:
 			axs.set_ylabel(r"Power ($n W$)")
 			plt.subplots_adjust(hspace=0.4)
 			figname="FineScan2.png"
-			#plt.savefig(figname)
 		n=n+1
 		time.sleep(3)
 except KeyboardInterrupt:
